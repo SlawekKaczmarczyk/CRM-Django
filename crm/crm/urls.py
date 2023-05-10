429188f868f5258e257eae726528bbc47ab9073d
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-05-10 08:59:33 +0200

    Creating pages

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views
+from django.urls import path, include
+
+from core.views import index, about
+from userprofile.views import signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('about/', about, name='about'),    
+    path('sign-up/', signup, name='signup'),    
+    path('log-in/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
+    path('log-out/', views.LogoutView.as_view(), name='logout'),
+    path('dashboard/', include('dashboard.urls')),
 ]
