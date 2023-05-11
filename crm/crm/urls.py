404e829c5ebe79e080e9436685d936301453bbf1
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-05-11 16:09:44 +0200

    Clients

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path('log-out/', views.LogoutView.as_view(), name='logout'),
     path('dashboard/', include('dashboard.urls')),
     path('dashboard/leads/', include('lead.urls')),
+    path('dashboard/clients/', include('client.urls')),
 ]
